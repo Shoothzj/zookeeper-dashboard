61895c6499f18c575a2a46ab4326c1b204649722
ZhangJian He <shoothzj@gmail.com>
2022-10-19 16:07:11 +0800

    fix unit tests

--- src/test/java/com/github/shoothzj/zdash/utils/DecodeUtilTest.java
+++ src/test/java/com/github/shoothzj/zdash/utils/DecodeUtilTest.java
@@ -1,19 +1,23 @@
 package com.github.shoothzj.zdash.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class DecodeUtilTest {
 
     @Test
     public void testDecodePulsarManagedLedgerTopic() throws Exception {
-        byte[] data = HexUtil.hexToByteArray("0a0508da01200");
-        DecodeUtil.decodeData(data, "Pulsar", "ManagedLedgerTopic");
+        byte[] data = HexUtil.hexToByteArray("0a0508da012000");
+        String content = DecodeUtil.decodeData(data, "Pulsar", "ManagedLedgerTopic");
+        log.info("content is\n{}", content);
     }
 
     @Test
     public void testDecodePulsarManagedLedgerSubscription() throws Exception {
         byte[] data = HexUtil.hexToByteArray("08db0110da0118eeeeeeeeeeeeeeeeee0130e09cdd-3bd30");
-        DecodeUtil.decodeData(data, "Pulsar", "ManagedManagedLedgerSubscription");
+        String content = DecodeUtil.decodeData(data, "Pulsar", "ManagedManagedLedgerSubscription");
+        log.info("content is\n{}", content);
     }
 
 
